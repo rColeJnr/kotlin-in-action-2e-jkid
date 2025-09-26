4d7907e920cec3ac70b3157891dba8e62b163018
rColeJnr <ricardo.m.jnr@gmail.com>
2025-09-26 08:49:40 -0700

    DSLs

--- src/test/kotlin/exercise/Map.kt
+++ src/test/kotlin/exercise/Map.kt
@@ -3,8 +3,10 @@ package exercise
 import kotlin.test.Test
 import kia.jkid.deserialization.deserialize
 import kia.jkid.serialization.serialize
-import kotlin.test.Ignore
+import org.junit.jupiter.api.assertThrows
 import kotlin.test.assertEquals
+import kotlin.test.assertFails
+import kotlin.test.assertFailsWith
 
 data class BookStore(val bookPrice: Map<String, Double>)
 
@@ -20,4 +22,127 @@ class MapTest {
     @Test fun testDeserialization() {
         assertEquals(bookStore, deserialize(json))
     }
+
+    @Test fun buildString() {
+
+
+       val one = with("1") {
+           println("ghgh$this")
+            toInt()
+        }
+        assertEquals(1, one)
+    }
+
+    @Test fun infixCopy() {
+        val str = "Could, Should, Does - Miracle" // Jim Rohn
+        assertEquals("$str$str", str copy CopiadoraString())
+    }
+
+    @Test fun infixQuote() {
+        val str = "Could, Should, Don't - Mistake collaso"
+        assertEquals("$str - Jim Rohn", str quote CopiadoraString())
+    }
+
+    @Test fun infixIntCopy() {
+        val int = 1
+        assertEquals(11, int copy CopiadoraInt())
+    }
+
+    @Test fun infixIntQuote() {
+        val int = 1
+        assertFails { int quote CopiadoraInt() }
+    }
+
+    @Test fun `extent primitive type`() {
+        val str = """
+            |Isn't this good stuff?, i am telling you, this stuff
+            |changed my life,
+            |turned me every way, but lose.
+            |It's so good i should have paid to get in.
+        """.trimMargin()
+
+        assertEquals("$str - quoting Jim Rohn", str.JimRohn)
+    }
+
+    @Test fun testReceiver() {
+        val greeter = Greeter()
+        greeter.missMom("Mother")
+        greeter {
+            missMom("Mother")
+            printSomething("Ricardo") {
+                isNullOrBlank()
+                this.equals("Ricardo")
+                JimRohn
+            }
+        }
+
+       greeter {
+            assertEquals( "I miss my Lumina",  missMom("Lumina"))
+        }
+
+
+
+    }
+
 }
+
+infix fun <T> T.quote(copiadora: Copiadora<T>) = copiadora.quoteJim(this)
+
+infix fun <T> T.copy(copiadora: Copiadora<T>) = copiadora.copia(this)
+
+interface Copiadora<T> {
+    fun copia(value: T): T
+    fun quoteJim(value: T): T
+}
+
+fun CopiadoraString(): Copiadora<String> {
+    return object: Copiadora<String> {
+        override fun copia(value: String): String {
+            return "$value$value"
+        }
+
+        override fun quoteJim(value: String): String {
+            return "$value - Jim Rohn"
+        }
+    }
+}
+
+fun CopiadoraInt(): Copiadora<Int> {
+    return object: Copiadora<Int> {
+        override fun copia(value: Int): Int {
+            return "$value$value".toInt()
+        }
+
+        override fun quoteJim(value: Int): Int {
+            throw AssertionError("Can't quote jim on Int type")
+        }
+    }
+}
+
+val String.JimRohn: String
+    get() = "$this - quoting Jim Rohn"
+
+class Greeter() {
+
+    fun missMom(name: String): String {
+        return "I miss my $name"
+    }
+
+    fun printSomething(value: String, body: String.() -> Unit) {
+       print("String Operations on $this")
+        body(value)
+    }
+    // operator invoke makes so that you can call a class, like a function, and executes the code in the invoke fun
+    operator fun invoke(body: Greeter.() -> Unit) {
+        body()
+    }
+
+}
+
+inline fun <T> T.apply(block: T.() -> Unit): T {
+    block()
+    return this
+} // returns the receiver
+
+inline fun <T, R> with(receiver: T, block: T.() -> R): R =
+    receiver.block() // returns the result of calling the lambda
